751648b3c22cbd98183fac229d597c5f65545fb5
Emanuel Carnevale <emanuel.carnevale@gmail.com>
2011-02-20 16:08:23 +0100

    adding auth token instead of email/password

--- run_example.rb
+++ run_example.rb
@@ -1,14 +1,14 @@
 CAMPFIRE_SUBDOMAIN = ''
-CAMPFIRE_EMAIL = ''
-CAMPFIRE_PASSWORD = '' 
 CAMPFIRE_ROOM = '' # the NAME of the room, case sensitive
+CAMPFIRE_API_TOKEN = '' #API TOKEN instead of EMAIL and PASSWORD AUTHENTICATION method
+
 FEED_URL = '' # from http://search.twitter.com
 CACHE_FILE = 'archived_latest.txt'
 OPTIONS = {} # {:twicture => true} # => posts tweets as Twictures
 
 require 'twitter2campfire'
 
-campfire = Tinder::Campfire.new CAMPFIRE_SUBDOMAIN
-campfire.login CAMPFIRE_EMAIL, CAMPFIRE_PASSWORD
+campfire = Tinder::Campfire.new(CAMPFIRE_SUBDOMAIN, :token => CAMPFIRE_API_TOKEN)
+
 t = Twitter2Campfire.new(FEED_URL, campfire, CAMPFIRE_ROOM, CACHE_FILE, OPTIONS)
 t.publish_entries
